b9a4a4082867f3e87adcc327638d70eb15c93a8d
PULAPALLI JASHUVADANIEL <98630530+21A91A0498@users.noreply.github.com>
2022-10-31 14:48:21 +0530

    def get_prfs(n):     s=0     for i in range(1,n):         if n%i==0:             s+=i     return s a=int(input()) b=int(input()) if get_prfs(a)==b and get_prfs(b)==a:     print("Amicable") else:     print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,15 +1,12 @@
+def get_prfs(n):
+    s=0
+    for i in range(1,n):
+        if n%i==0:
+            s+=i
+    return s
 a=int(input())
 b=int(input())
-pfsa=0
-pfsb=0
-for i in range(1,a):
-    if a%i==0:
-        pfsa=pfsa+i
-for i in range(1,b):
-    if b%i==0:
-        pfsb+=i
-if pfsb==a and pfsa==b:
-    print('Amicable')
+if get_prfs(a)==b and get_prfs(b)==a:
+    print("Amicable")
 else:
     print("Not Amicable")
-        
