d307aad26291480f0c314115d277e383beed927d
PULAPALLI JASHUVADANIEL <98630530+21A91A0498@users.noreply.github.com>
2022-11-01 21:30:56 +0530

    n=int(input()) sum=0 pro=1 while n:     d=n%10     sum=sum+d     pro=pro*d     n=n//10 if pro==sum:     print('Spy Number') else:     print('Not Spy Number')

--- Spy_Number.py
+++ Spy_Number.py
@@ -1,12 +1,12 @@
 n=int(input())
 sum=0
-p=1
+pro=1
 while n:
     d=n%10
     sum=sum+d
-    p=p*d
+    pro=pro*d
     n=n//10
-if p==sum:
+if pro==sum:
     print('Spy Number')
 else:
-    -print('Not Spy Number')
+    print('Not Spy Number')
