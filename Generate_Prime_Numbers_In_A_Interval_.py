2128d6fae97b3f0849da1ea88b7fc3f55506d357
PULAPALLI JASHUVADANIEL <98630530+21A91A0498@users.noreply.github.com>
2022-11-02 14:03:47 +0530

    n=int(input()) m=int(input()) while n<=m:     i=1     temp=n     count=0     while i<=temp:         if temp%i==0:             count+=1         i+=1     if count==2:         print(n)     n+=1

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -2,11 +2,14 @@ n=int(input())
 m=int(input())
 while n<=m:
     i=1
+    temp=n
     count=0
-    while i<=n:
-        if n%i==0:
+    while i<=temp:
+        if temp%i==0:
             count+=1
         i+=1
     if count==2:
         print(n)
     n+=1
+        
+        
