bdb3bbbea8706160b3c3fd4d953e15c9a1211928
PULAPALLI JASHUVADANIEL <98630530+21A91A0498@users.noreply.github.com>
2022-12-02 14:53:19 +0530

    import math def prime(n):     sq=int(math.sqrt(n))     for i in range(2,sq+1):         if n%i==0:             return False     return True t=int(input()) i=0 while i<t:
        n=int(input())
        p=n+1
        temp=n
        m=n
        while m<=n:
            if prime(m):
                k=m
                break
            m-=1
        while p>n:
            if prime(p):
                g=p
                break
            p+=1
        if abs(k-temp)>abs(g-temp):
            print(g)
        elif abs(k-temp)<abs(g-temp):
            print(k)
        else:
            print(k)
        i+=1

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,28 +1,32 @@
 import math
 def prime(n):
-    
     sq=int(math.sqrt(n))
     for i in range(2,sq+1):
-        if n%i==0: 
+        if n%i==0:
             return False
     return True
-            
-for i in range(int(input())):
-    n=int(input())
-    temp=n
-    for i in range(n,0,-1):
-        if prime(i):
-            k=i
-            break
-    for i in range(n+1,n*n*n):
-        if prime(i):
-            g=i
-            break
+t=int(input())
+i=0             
+while i<t:
 
+    n=int(input())
+    p=n+1
+    temp=n
+    m=n
+    while m<=n:
+        if prime(m):
+            k=m
+            break
+        m-=1
+    while p>n:
+        if prime(p):
+            g=p
+            break
+        p+=1
     if abs(k-temp)>abs(g-temp):
         print(g)
-    elif abs(k-temp)<=abs(g-temp):
+    elif abs(k-temp)<abs(g-temp):
         print(k)
-    elif abs(k-temp)==abs(g-temp):
+    else:
         print(k)
-    
+    i+=1
