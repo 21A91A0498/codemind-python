6f232c16ded3ca62f057e91a70dad4f3dd5f3e83
PULAPALLI JASHUVADANIEL <98630530+21A91A0498@users.noreply.github.com>
2023-05-23 21:53:35 +0530

    n,m=map(int,input().split()) l=list(map(int,input().split())) k=list(map(int,input().split())) ll=[] for i in l:     if i not in k:         ll.append(i) for i in k:     if i not in l:         ll.append(i) s=list(set(ll)) c=len(s) print(c)

---
+++ Count_elements_II.py
@@ -0,0 +1,13 @@
+n,m=map(int,input().split())
+l=list(map(int,input().split()))
+k=list(map(int,input().split()))
+ll=[]
+for i in l:
+    if i not in k:
+        ll.append(i)
+for i in k:
+    if i not in l:
+        ll.append(i)
+s=list(set(ll))
+c=len(s)
+print(c)
