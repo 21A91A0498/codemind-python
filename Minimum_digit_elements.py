c6a991e339df4cbe8b5043111f30af087433e0c2
PULAPALLI JASHUVADANIEL <98630530+21A91A0498@users.noreply.github.com>
2023-06-27 20:12:52 +0530

    def digicount(n):     c=0     while n:         d=n%10         c+=1         n=n//10     return c n=int(input()) l=list(map(int,input().split())) d={} for i in l:     k=digicount(i)     d[i]=k c=0 for i,j in d.items():     if j==min(d.values()):         c+=1 print(c)

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,18 @@
+def digicount(n):
+    c=0
+    while n:
+        d=n%10
+        c+=1
+        n=n//10
+    return c
+n=int(input())
+l=list(map(int,input().split()))
+d={}
+for i in l:
+    k=digicount(i)
+    d[i]=k
+c=0
+for i,j in d.items():
+    if j==min(d.values()):
+        c+=1
+print(c)
